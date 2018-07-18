14b39635eb11b7d8d3a51f1ed02e13f1d17ae031
Unknown <ariasmaxi89@gmail.com>
2018-07-18 00:37:56 -0300

    complex types folder and skeleton

---
+++ 4 - Data Types/4.1 - Complex types/ComplexTypes.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+contract ComplexTypes {
+    function ComplexTypes(){
+        // TODO: Strings
+        // TODO: structs
+        // TODO: Arrays
+        // TODO: Mappings
+    }
+}
